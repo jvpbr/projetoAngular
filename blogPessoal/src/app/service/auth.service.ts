2b6511669991b10a3d64ef5e38e65908e23e712c
unknown <joseparanan@gmail.com>
2021-05-10 18:58:45 -0300

    Implementação post e get de postagens

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -16,12 +16,17 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+
   entrar(userLogin: UserLogin): Observable<UserLogin>{
     return this.http.post<UserLogin>(`${this.server}/usuarios/logar`, userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>(`${this.server}/usuarios/cadastrar`, user)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`${this.server}/usuarios/${id}`)
   }
 
   logado(){
